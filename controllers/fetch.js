4b42f279aec753049321e5fc4d746bf00b730017
Jon C <jchan178@gmail.com>
2018-03-16 21:46:17 -0500

    minor edits, not working

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -1,14 +1,14 @@
 import { model } from "mongoose";
 
-var express = require("express");
+// var express = require("express");
 
-var router = express.Router();
+// var router = express.Router();
 
-var request = require("request");
+// var request = require("request");
 
-var cheerio = require("cheerio");
+// var cheerio = require("cheerio");
 
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
--- server.js
+++ server.js
@@ -1,39 +1,36 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
 
 var app = express();
-var port = process.env.port || 3000;
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(methodOverride('_method'));
+//app.use(methodOverride('_method'));
 
 // Our scraping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: false }));
-// Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
 
-// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/week18Populater", {
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
 // Require all models
 var db = require("./models");
 
+var PORT = 3000;
 // Set Handlebars as the view engine
 var exphbs = require("express-handlebars");
 
